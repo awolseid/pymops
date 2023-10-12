5a60c3a6209666d911bde517868ed755c33ec96d
Awol Seid Ebrie <88894209+awolseid@users.noreply.github.com>
2023-10-12 09:06:35 +0900

    Add files via upload

---
+++ pymops/define_dqn.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class DQNet(nn.Module):
+    def __init__(self, environ, hidden_nodes):
+        super(DQNet, self).__init__()
+        self.environ = environ
+        self.state_vec, _ = self.environ.reset()
+        self.input_nodes = len(self.state_vec)
+        self.output_nodes = 2 * self.environ.n_units
+        
+        self.model = nn.Sequential(
+            nn.Linear(self.input_nodes, hidden_nodes),
+            nn.ReLU(),
+            nn.Linear(hidden_nodes, self.output_nodes),
+            nn.ReLU()
+        )
+    
+    def forward(self, state_vec):
+        return self.model(torch.as_tensor(state_vec).float())
